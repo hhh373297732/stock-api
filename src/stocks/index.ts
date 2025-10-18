abba7b932db36e0cd1a928f5b2601eaaa7c82790
Niko <hhh373297732@163.com>
2025-10-18 12:05:24 +0800

    delete: 网易接口已失效，删除

--- src/stocks/index.ts
+++ src/stocks/index.ts
@@ -2,15 +2,13 @@
 import base from "./base";
 import sina from "./sina";
 import xueqiu from "./xueqiu";
-import netease from "./netease";
 import tencent from "./tencent";
 
-export { base, sina, xueqiu, netease, tencent };
+export { base, sina, xueqiu, tencent };
 
 export default {
   base,
   sina,
   xueqiu,
-  netease,
   tencent,
 };
