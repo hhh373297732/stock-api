33ae647c391bf92cffb14f041238cd36d8400695
Niko <hhh373297732@163.com>
2025-10-18 16:26:13 +0800

    fix：回滚提交

--- src/stocks/index.ts
+++ src/stocks/index.ts
@@ -2,13 +2,15 @@
 import base from "./base";
 import sina from "./sina";
 import xueqiu from "./xueqiu";
+import netease from "./netease";
 import tencent from "./tencent";
 
-export { base, sina, xueqiu, tencent };
+export { base, sina, xueqiu, netease, tencent };
 
 export default {
   base,
   sina,
   xueqiu,
+  netease,
   tencent,
 };
